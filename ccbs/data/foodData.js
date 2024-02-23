86467dc7e5d849c5cd314be343ee8d8eab4cfea7
ksh3760 <oksh3760@naver.com>
2024-02-23 17:28:12 +0900

    Update food data
    update image url

--- ccbs/data/foodData.js
+++ ccbs/data/foodData.js
@@ -1,117 +1,117 @@
 var foodList = [
     {
         "name": "먹지마",
-        "img": "https://img.freepik.com/premium-vector/no-eating-sign-vector-illustration_118339-3129.jpg",
+        "img": "https://raw.githubusercontent.com/ksh3760/imgStorage/main/doNotEat.png",
         "tag": []
     },
     {
         "name": "국밥",
-        "img": "https://gift.hmart.com/media/catalog/product/cache/0c75b0d6b380af6dca3ec180c127a709/f/i/file_30_1.jpg",
+        "img": "https://raw.githubusercontent.com/ksh3760/imgStorage/main/gukBap.jpg",
         "tag": ["밥", "한식", "고기", "따뜻한", "국물", "해장"]
     },
     {
         "name": "제육",
-        "img": "https://i.namu.wiki/i/npjMucg7sLxIm8Uca8O3lygeM9UX2Dsu4RVnVxcDdaItsLZ6w0N0Ju54gVqn8O7r7taBR6bAEwL9qOLoUKKbzg.webp",
+        "img": "https://raw.githubusercontent.com/ksh3760/imgStorage/main/jeYukBokkeum.png",
         "tag": ["밥", "한식", "고기", "볶음"]
     },
     {
         "name": "김밥",
-        "img": "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0e/Gimbap_%28pixabay%29.jpg/330px-Gimbap_%28pixabay%29.jpg",
+        "img": "https://raw.githubusercontent.com/ksh3760/imgStorage/main/kimBop.jpg",
         "tag": ["밥", "한식", "야채"]
     },
     {
         "name": "백반",
-        "img": "https://i.namu.wiki/i/hyytrAYkRz96Vh-ekWQTEX_s-IjG7W0IodxFfI-kwsmCK8XHbF1bdb7EXXCB08Eiv5JWUUC6xK4Qvt6m0nc7Mg.webp",
+        "img": "https://raw.githubusercontent.com/ksh3760/imgStorage/main/hundredHalf.png",
         "tag": ["밥", "한식", "고기", "야채", "국물"]
     },
     {
         "name": "회덮밥",
-        "img": "https://i.namu.wiki/i/nZjLOBNAh81YbDJwdPfyypUR51LfGglO-vNaHOiMVchwwMSe7cYvWO3T6aQ8VcQkhPLKUq1kqK2SEU6_fmZHbgsxgXu41wUJfHA9e2BGOO4KcYoMLGdzHZeIskyxfQ1Qv82QRBOl6RmIdtCR3GiM3g.webp",
+        "img": "https://raw.githubusercontent.com/ksh3760/imgStorage/main/hoedeopBap.jpg",
         "tag": ["밥", "한식", "생선", "야채"]
     },
     {
         "name": "냉면",
-        "img": "https://upload.wikimedia.org/wikipedia/commons/thumb/9/92/Dongmu_Bapsang_02.jpg/1200px-Dongmu_Bapsang_02.jpg",
+        "img": "https://raw.githubusercontent.com/ksh3760/imgStorage/main/naengmyeon.jpg",
         "tag": ["면", "한식", "시원한", "국물"]
     },
     {
         "name": "부대찌개",
-        "img": "https://i.namu.wiki/i/Ni7XpClHeRIcHn31TU5uanomCAmT0YcgV3GuOUGNdW1Td6UpW79EzpSCI7aW2NvzaZHAEIzKvGijmPPluC4qIw.webp",
+        "img": "https://raw.githubusercontent.com/ksh3760/imgStorage/main/buDaeChigae.jpg",
         "tag": ["한식", "국물", "따뜻한", "매운", "해장"]
     },
     {
         "name": "짜장면",
-        "img": "https://i.namu.wiki/i/j2AxLP9AtrcJebh4DVfGxowfXwI3a95dG_YZb_Ktczc6Ca7ACyd_NJL3YHQMw8SABGTQiJDwSpySOSSBLZVEZw.webp",
+        "img": "https://raw.githubusercontent.com/ksh3760/imgStorage/main/zaZangMyeon.jpg",
         "tag": ["면", "중식", "고기", "야채"]
     },
     {
         "name": "짬뽕",
-        "img": "https://upload.wikimedia.org/wikipedia/commons/4/44/Jjampong.JPG",
+        "img": "https://raw.githubusercontent.com/ksh3760/imgStorage/main/zamBbong.jpg",
         "tag": ["면", "중식", "국물", "매운", "해장"]
     },
     {
         "name": "볶음밥",
-        "img": "https://upload.wikimedia.org/wikipedia/commons/thumb/5/59/Chinese_fried_rice.jpg/800px-Chinese_fried_rice.jpg",
+        "img": "https://raw.githubusercontent.com/ksh3760/imgStorage/main/friedRice.jpg",
         "tag": ["밥", "중식", "고기", "야채", "볶음"]
     },
     {
         "name": "리소토",
-        "img": "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Risotto_Alla_Marinara.jpg/800px-Risotto_Alla_Marinara.jpg?20070115000954",
+        "img": "https://raw.githubusercontent.com/ksh3760/imgStorage/main/risotto.jpg",
         "tag": ["밥", "양식", "야채", "따뜻한"]
     },
     {
         "name": "햄버거",
-        "img": "https://static.wikia.nocookie.net/spongebob/images/2/2f/Krusty_Krab_Training_Video_081.png",
+        "img": "https://raw.githubusercontent.com/ksh3760/imgStorage/main/krabbyPatty.jpg",
         "tag": ["빵", "양식", "고기", "야채"]
     },
     {
         "name": "파스타",
-        "img": "https://www.foodandwine.com/thmb/c-MBu_vMHq3EcoN_KPxwg-oZjKo=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/Pasta-Aglio-E-Olio-2-FT-RECIPE0123-38cd2045646a4635a80e8166f085fc7e.jpg",
+        "img": "https://raw.githubusercontent.com/ksh3760/imgStorage/main/pasta.jpg",
         "tag": ["면", "양식", "야채", "따뜻한"]
     },
     {
         "name": "라면",
-        "img": "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e4/Jin_Ramen_%28spicy%29_20210731_002.jpg/800px-Jin_Ramen_%28spicy%29_20210731_002.jpg",
+        "img": "https://raw.githubusercontent.com/ksh3760/imgStorage/main/ramyeon.jpg",
         "tag": ["면", "한식", "국물", "따뜻한"]
     },
     {
         "name": "스시",
-        "img": "https://gurunavi.com/ko/japanfoodie/article/sushi/img/sushi_01.jpg",
+        "img": "https://raw.githubusercontent.com/ksh3760/imgStorage/main/sushi.jpg",
         "tag":  ["밥", "일식", "생선"]
     },
     {
         "name": "라멘",
-        "img": "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ec/Shoyu_ramen%2C_at_Kasukabe_Station_%282014.05.05%29_1.jpg/1024px-Shoyu_ramen%2C_at_Kasukabe_Station_%282014.05.05%29_1.jpg",
+        "img": "https://raw.githubusercontent.com/ksh3760/imgStorage/main/ramen.jpg",
         "tag": ["면", "일식", "따뜻한", "국물"]
     },
     {
         "name": "소바",
-        "img": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQf1cVaqPVywx1jSwvkIGzQ-lZPqX2EuhEyED9DkUVBZA&s",
+        "img": "https://raw.githubusercontent.com/ksh3760/imgStorage/main/soba.jpg",
         "tag": ["면", "일식", "시원한", "국물"]
     },
     {
         "name": "텐동",
-        "img": "https://static.wtable.co.kr/image/production/service/recipe/719/23757871-daf5-4a3d-b6e3-87b0a975f8d8.jpg?size=800x800",
+        "img": "https://raw.githubusercontent.com/ksh3760/imgStorage/main/10don.jpg",
         "tag": ["밥", "일식", "야채", "튀김"]
     },
     {
         "name": "돈가스",
-        "img": "https://upload.wikimedia.org/wikipedia/commons/thumb/6/62/Tonkatsu_by_ayustety_in_Tokyo.jpg/250px-Tonkatsu_by_ayustety_in_Tokyo.jpg",
+        "img": "https://raw.githubusercontent.com/ksh3760/imgStorage/main/tonkatsu.jpg",
         "tag": ["밥", "양식", "고기", "튀김"]
     },
     {
         "name": "마라탕",
-        "img": "https://i.namu.wiki/i/qFWfOHBd0mx7NmNquwtaSbUjnPumXpk5oi1jxNKpWUsv_eGJe44xm9AePkbhQ6hIxTjMtroFaOFPbhBy0MSbNQ.webp",
+        "img": "https://raw.githubusercontent.com/ksh3760/imgStorage/main/maratang.jpg",
         "tag": ["중식", "국물", "따뜻한", "매운", "해장"]
     },
     {
         "name": "마라샹궈",
-        "img": "https://src.hidoc.co.kr/image/lib/2019/7/22/20190722101147715_0.jpg",
+        "img": "https://raw.githubusercontent.com/ksh3760/imgStorage/main/maraXiangguo.jpg",
         "tag": ["중식", "볶음", "따뜻한", "매운"]
     },
     {
         "name": "만두",
-        "img": "https://i.namu.wiki/i/syBMO9rzc1yUO-Tg2ARORWHspZHMct1uiOEOuuXVBU135mH-0Z03rAd6x_XIkpuslrV1HHd-C-oUVEw9MGXpmQ.webp",
+        "img": "https://raw.githubusercontent.com/ksh3760/imgStorage/main/dumpling.jpg",
         "tag": ["빵", "중식", "고기", "야채", "찜"]
     }
 ]
